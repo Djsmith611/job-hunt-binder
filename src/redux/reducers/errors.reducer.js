cc32d18f54e486f1483c9c756835e2192aeb0b2e
David Smith <djsmith611@me.com>
2024-06-04 14:27:03 -0700

    updated errors reducer

--- src/redux/reducers/errors.reducer.js
+++ src/redux/reducers/errors.reducer.js
@@ -1,40 +1,50 @@
-import { combineReducers } from 'redux';
+import {
+  CLEAR_LOGIN_ERROR,
+  LOGIN_FAILURE,
+  LOGIN_FAILURE_NO_CODE,
+  LOGIN_INPUT_ERROR,
+} from "../../modules/actions/loginActions";
+import {
+  CLEAR_REGISTRATION_ERROR,
+  REGISTRATION_INPUT_ERROR,
+  REGISTRATION_FAILURE,
+} from "../../modules/actions/registrationActions";
+import { combineReducers } from "redux";
 
-// loginMessage holds the string that will display
-// on the login screen if there's an error
-const loginMessage = (state = '', action) => {
+const initialState = "";
+
+// loginMessage for login form
+const loginMessage = (state = initialState, action) => {
+  // state.errors.loginMessage
   switch (action.type) {
-    case 'CLEAR_LOGIN_ERROR':
-      return '';
-    case 'LOGIN_INPUT_ERROR':
-      return 'Enter your username and password!';
-    case 'LOGIN_FAILED':
-      return "Oops! The username and password didn't match. Try again!";
-    case 'LOGIN_FAILED_NO_CODE':
-      return 'Oops! Something went wrong! Is the server running?';
+    case CLEAR_LOGIN_ERROR:
+      return initialState;
+    case LOGIN_INPUT_ERROR:
+      return "Enter your email and password!";
+    case LOGIN_FAILURE:
+      return "Oops! The email and password didn't match. Try again!";
+    case LOGIN_FAILURE_NO_CODE:
+      return "Oops! Something went wrong! Is the server running?";
     default:
       return state;
   }
 };
 
-// registrationMessage holds the string that will display
-// on the registration screen if there's an error
-const registrationMessage = (state = '', action) => {
+// registrationMessage for registration form
+const registrationMessage = (state = initialState, action) => {
+  // state.errors.registrationMessage
   switch (action.type) {
-    case 'CLEAR_REGISTRATION_ERROR':
-      return '';
-    case 'REGISTRATION_INPUT_ERROR':
-      return 'Choose a username and password!';
-    case 'REGISTRATION_FAILED':
-      return "Oops! That didn't work. The username might already be taken. Try again!";
+    case CLEAR_REGISTRATION_ERROR:
+      return initialState;
+    case REGISTRATION_INPUT_ERROR:
+      return "Please fill required fields!";
+    case REGISTRATION_FAILURE:
+      return "Oops! That didn't work. Try again!";
     default:
       return state;
   }
 };
 
-// make one object that has keys loginMessage, registrationMessage
-// these will be on the redux state at:
-// state.errors.loginMessage and state.errors.registrationMessage
 export default combineReducers({
   loginMessage,
   registrationMessage,
