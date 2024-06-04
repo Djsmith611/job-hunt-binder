7e85b4e1aa2beb49ee372f525f78edb6189a9874
David Smith <djsmith611@me.com>
2024-06-04 14:39:14 -0700

    updated root reducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import errors from './errors.reducer';
-import user from './user.reducer';
+import { combineReducers } from "redux";
+import errors from "./errors.reducer";
+import user from "./user.reducer";
+import leads from "./leads.reducer";
 
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
+  leads, // will have lead information
 });
 
 export default rootReducer;
