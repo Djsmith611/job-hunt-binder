7f3a5b1655dee44631c1a37f5bc06a30e677a6d5
David Smith <djsmith611@me.com>
2024-06-05 15:24:42 -0700

    updated user router with previous

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -5,44 +5,52 @@ const {
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 const userStrategy = require('../strategies/user.strategy');
+const createUserDirectory = require('../modules/userDirectory');
 
 const router = express.Router();
 
-// Handles Ajax request for user information if user is authenticated
 router.get('/', rejectUnauthenticated, (req, res) => {
-  // Send back user object from the session (previously queried from the database)
   res.send(req.user);
 });
 
-// Handles POST request with new user data
-// The only thing different from this and every other post we've seen
-// is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {
-  const username = req.body.username;
-  const password = encryptLib.encryptPassword(req.body.password);
+  const { email, password, f_name, l_name } = req.body;
+  const encryptedPassword = encryptLib.encryptPassword(password);
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `
+    INSERT INTO "user" ("email", "password", "f_name", "l_name")
+    VALUES ($1, $2, $3, $4) RETURNING "id";
+  `;
+
   pool
-    .query(queryText, [username, password])
-    .then(() => res.sendStatus(201))
+    .query(queryText, [email, encryptedPassword, f_name, l_name])
+    .then(async (result) => {
+      const userId = result.rows[0].id;
+      try{
+        const directoryUrl = await createUserDirectory(userId);
+        const updateQueryText = `
+          UPDATE "user" 
+          SET "s3_dir" = $1
+          WHERE "id" = $2;
+        `;
+        await pool.query(updateQueryText, [directoryUrl, userId]);
+        res.sendStatus(201);
+      } catch (err){
+        console.error('Error creating directory or updating user:', err);
+        res.sendStatus(500);
+      }
+    })
     .catch((err) => {
       console.log('User registration failed: ', err);
       res.sendStatus(500);
     });
 });
 
-// Handles login form authenticate/login POST
-// userStrategy.authenticate('local') is middleware that we run on this route
-// this middleware will run our POST if successful
-// this middleware will send a 404 if not successful
 router.post('/login', userStrategy.authenticate('local'), (req, res) => {
   res.sendStatus(200);
 });
 
-// clear all server session information about this user
 router.post('/logout', (req, res, next) => {
-  // Use passport's built-in method to log out the user
   req.logout((err) => {
     if (err) { return next(err); }
     res.sendStatus(200);
